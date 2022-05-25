6115b508a52b1e7f4af8b99a540eb2bf520257dc
MariiMariis <mariana.motta94@gmail.com>
2022-05-24 21:55:06 -0300

    Finalizar projeto - Funcionalidades do banco

---
+++ Aula2.Fiap.Bank/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Aula2.Fiap.Bank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ler os dados imputados pelo usuário
+
+            Console.WriteLine("Digite seu nome completo:");
+            var nome = Console.ReadLine();
+
+            Console.WriteLine("Digite seu CPF:");
+            var cpf = Console.ReadLine();
+
+            Console.WriteLine("Digite o telefone do cliente:");
+            var tel = Console.ReadLine();
+
+
+            //TODO: FINALIZAR INSERT DE DADOS DA CONTA E MÉTODOS PARA TRANSAÇÕES
+        }
+    }
+}
